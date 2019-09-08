9970de40b7d08e8750d6605dcf0f24d940dbe8fd
aemarco <4aemarco@gmail.com>
2019-09-08 02:41:51 +0200

    added logging stuff

---
+++ Contracts/Enums.cs
@@ -0,0 +1,11 @@
+﻿namespace Contracts
+{
+    public enum LogLevel
+    {
+        Debug,
+        Info,
+        Warn,
+        Error,
+        Fatal
+    }
+}
---
+++ Contracts/Messages/LogMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Contracts.Messages
+{
+    public class LogMessage
+    {
+        public DateTimeOffset Timestamp { get; set; }
+        public string Environment { get; set; }
+        public string App { get; set; }
+        public string Source { get; set; }
+        public LogLevel LogLevel { get; set; }
+        public string Message { get; set; }
+        public Exception Exception { get; set; }
+
+    }
+
+
+
+
+}
