f03e6cb6dbb13fdcaee5d2d3c664cf66aec85a1d
aemarco <4aemarco@gmail.com>
2019-09-08 04:02:31 +0200

    added wallpaperdownloader stuff

---
+++ Contracts/Api/RequestObjects/DownloadableCategoryReq.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Contracts.Api.RequestObjects
+{
+    public class DownloadableCategoryReq
+    {
+        public string Cheat { get; set; }
+        public Version Version { get; set; }
+    }
+}
---
+++ Contracts/Api/RequestObjects/ExtendedCategory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Contracts.Api.RequestObjects
+{
+    public class ExtendedCategory
+    {
+        public int Id { get; set; }
+        public string CategoryString { get; set; }
+        public string Display { get; set; }
+        public string Icon { get; set; }
+        public List<ExtendedCategory> SubCategories { get; set; }
+        public bool IsMainCategory { get; set; }
+
+        //logic
+        public bool FSK_Relevat
+        {
+            get
+            {
+                return
+                  CategoryString.StartsWith("Girls") ||
+                  CategoryString.StartsWith("Men") ||
+                  CategoryString == string.Empty;
+            }
+        }
+
+    }
+}
