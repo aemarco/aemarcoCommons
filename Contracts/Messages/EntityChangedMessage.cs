043fa4ec100d88f447bd9fc52f3601139a3779c7
aemarco <4aemarco@gmail.com>
2019-09-13 21:45:43 +0200

    contract EntityChangedMessage now has Sender

---
+++ Contracts/Messages/EntityChangedMessage.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace Contracts.Messages
+{
+    public class EntityChangedMessage
+    {
+        public string Sender { get; set; }
+        public string Type { get; set; }
+        public string State { get; set; }
+        public int Id { get; set; }
+
+        [JsonIgnore]
+        public object EntityObject { get; set; }
+    }
+}
