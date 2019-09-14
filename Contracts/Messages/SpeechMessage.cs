f993079519169f0fab21f896d3edce080c191488
aemarco <4aemarco@gmail.com>
2019-09-14 20:30:01 +0200

    added speechmessage

---
+++ Contracts/Messages/SpeechMessage.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace Contracts.Messages
+{
+    public class SpeechMessage
+    {
+        public string Target { get; set; }
+        public string MessageType { get; set; }
+
+
+        public string Key { get; set; }
+        public int Value { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+    }
+}
