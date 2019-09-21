5b4a1c8dc6066f122b975b85b35cc36874d6731d
aemarco <4aemarco@gmail.com>
2019-09-21 23:13:50 +0200

    added FileServiceMessage

---
+++ Contracts/Messages/FileServiceMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace Contracts.Messages
+{
+    public class FileServiceMessage
+    {
+        public string ServiceType { get; set; }
+        public string EntryType { get; set; }
+        public int EntryId { get; set; }
+
+    }
+}
