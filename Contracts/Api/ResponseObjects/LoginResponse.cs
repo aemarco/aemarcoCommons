059487e9526e85a914820ab987b606b70dcc2a05
aemarco <4aemarco@gmail.com>
2019-09-27 23:17:17 +0200

    added loginresponse

---
+++ Contracts/Api/ResponseObjects/LoginResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Contracts.Api.ResponseObjects
+{
+    public class LoginResponse
+    {
+        public string UserId { get; set; }
+        public bool SendLog { get; set; }
+        public bool CollectInfos { get; set; }
+
+    }
+}
