7547aabdbd5a1c9c28d399f95784acb39accf823
aemarco <4aemarco@gmail.com>
2019-10-13 20:56:59 +0200

    added constants

---
+++ Contracts/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Contracts
+{
+    public static class Constants
+    {
+        public static string WEBGIRLS_URL = "https://aemarco.myds.me";
+
+
+
+
+        //video
+        public static int TIME_UNTIL_VIDEOVIEW_COUTS_AS_PLAYED_SECONDS = 15;
+
+    }
+}
