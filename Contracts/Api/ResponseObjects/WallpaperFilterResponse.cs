8a3592b3f099ccde460ab1e57637f11debddda09
aemarco <4aemarco@gmail.com>
2019-10-16 19:17:26 +0200

    added wall response

---
+++ Contracts/Api/ResponseObjects/WallpaperFilterResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Contracts.Api.ResponseObjects
+{
+    public class WallpaperFilterResponse
+    {
+        //for command
+        public string MonitorName { get; set; }
+        public bool IsFavorite { get; set; }
+        public bool FromCache { get; set; }
+        public WallpaperInfo Wallpaper { get; set; }
+
+        //for stats
+        public int WallsCount { get; set; }
+    }
+}
