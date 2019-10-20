da5e0722ff6641ad3030847c1dd13eae68b3fd5d
aemarco <4aemarco@gmail.com>
2019-10-20 04:24:54 +0200

    added extendedGirl

---
+++ Contracts/Api/ResponseObjects/ExtendedGirl.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Contracts.Api.ResponseObjects
+{
+    public class ExtendedGirl
+    {
+        public int Id { get; set; }
+        public string DisplayName { get; set; }
+
+    }
+}
