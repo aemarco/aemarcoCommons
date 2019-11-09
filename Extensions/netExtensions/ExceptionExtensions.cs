f3b473ee6ed956e2d5eff1359761684d8e2cf713
aemarco <4aemarco@gmail.com>
2019-11-09 23:11:03 +0100

    added exception extension

---
+++ Extensions/netExtensions/ExceptionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extensions.netExtensions
+{
+    public static class ExceptionExtensions
+    {
+
+        public static Exception Unpack(this Exception exception)
+        {
+            var result = exception;
+            while (result?.InnerException != null)
+            {
+                result = result.InnerException;
+            }
+            return result;
+        }
+
+
+
+
+    }
+}
