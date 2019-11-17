cdcd329424c74f4aed9f53c42846cf453490eed0
aemarco <4aemarco@gmail.com>
2019-11-17 18:09:04 +0100

    improved logmessage

--- Contracts/Messages/LogMessage.cs
+++ Contracts/Messages/LogMessage.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Contracts.Messages
 {
@@ -13,32 +14,39 @@ namespace Contracts.Messages
         public string Source { get; set; }
         public LogLevel LogLevel { get; set; }
         public string Message { get; set; }
-        public Dictionary<string, string> ExceptionDict { get; set; }
+        //optionals
+        public string ExceptionBase64 { get; set; }
         public string AdditionalBase64Info { get; set; }
 
 
 
+        //conveniance
         [JsonIgnore]
-        public Exception Exception
+        public string Exception
         {
+            get
+            {
+                return (String.IsNullOrWhiteSpace(ExceptionBase64)) ? null : Encoding.UTF8.GetString(Convert.FromBase64String(ExceptionBase64));
+            }
             set
             {
-                if (value != null)
-                {
-                    ExceptionDict = new Dictionary<string, string>
-                    {
-                        {"Type", value.GetType().ToString()},
-                        {"Message", value.Message},
-                        {"StackTrace", value.StackTrace}
-                    };
-                    foreach (DictionaryEntry data in value.Data)
-                        ExceptionDict.Add(data.Key.ToString(), data.Value.ToString());
-
-                }
+                if (value == null) ExceptionBase64 = null;
+                else ExceptionBase64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(value));
             }
-
         }
-
+        [JsonIgnore]
+        public string AdditionalInfo
+        {
+            get
+            {
+                return (String.IsNullOrWhiteSpace(AdditionalBase64Info)) ? null : Encoding.UTF8.GetString(Convert.FromBase64String(AdditionalBase64Info));
+            }
+            set
+            {
+                if (value == null) AdditionalBase64Info = null;
+                else AdditionalBase64Info = Convert.ToBase64String(Encoding.UTF8.GetBytes(value));
+            }
+        }
     }
 
 
