82503a8e8417b307b384cdfd86d2af7f435c6b6f
aemarco <4aemarco@gmail.com>
2019-11-17 23:24:50 +0100

    added natural sort tool

---
+++ Toolbox/SortTools/NaturalSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+
+namespace Toolbox.SortTools
+{
+    [SuppressUnmanagedCodeSecurity]
+    internal static class SafeNativeMethods
+    {
+        [DllImport("shlwapi.dll", CharSet = CharSet.Unicode)]
+        public static extern int StrCmpLogicalW(string psz1, string psz2);
+    }
+
+    public sealed class NaturalStringComparer : IComparer<string>
+    {
+        public int Compare(string a, string b)
+        {
+            return SafeNativeMethods.StrCmpLogicalW(a, b);
+        }
+    }
+
+    public sealed class NaturalFileInfoNameComparer : IComparer<FileInfo>
+    {
+        public int Compare(FileInfo a, FileInfo b)
+        {
+            return SafeNativeMethods.StrCmpLogicalW(a.Name, b.Name);
+        }
+    }
+}
