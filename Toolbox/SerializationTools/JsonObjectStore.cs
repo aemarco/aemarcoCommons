f604900b0e086e619f67f8b0ab9483d37cf23185
aemarco <4aemarco@gmail.com>
2019-12-28 02:12:46 +0100

    JsonObjectSore

---
+++ Toolbox/SerializationTools/JsonObjectStore.cs
@@ -0,0 +1,133 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Toolbox.SerializationTools
+{
+    public class JsonObjectStore<T>
+        where T : class
+    {
+        #region props
+
+        public string Filepath { get; set; }
+        public DateTimeOffset Generated { get; set; } = DateTimeOffset.Now;
+        public DateTimeOffset LastSaved { get; set; } = DateTimeOffset.MinValue;
+        public Dictionary<string, T> Storage { get; set; } = new Dictionary<string, T>();
+
+        #endregion
+
+        #region Handling entries
+
+        public void AddOrUpdateEntry(string key, T entry)
+        {
+            if (!Storage.ContainsKey(key))
+            {
+                Storage.Add(key, entry);
+            }
+            else
+            {
+                Storage[key] = entry;
+            }
+        }
+
+        public List<string> GetAllKeys()
+        {
+            return Storage.Keys.ToList();
+        }
+
+        public List<T> GetAllValues()
+        {
+            return Storage.Values.ToList();
+        }
+
+        public void RemoveEntry(string key)
+        {
+            if (key != null && Storage.ContainsKey(key))
+            {
+                Storage.Remove(key);
+            }
+        }
+
+        public void RemoveEntry(T entry)
+        {
+            var key = Storage.FirstOrDefault(x => x.Value == entry).Key;
+            if (key != null && Storage.ContainsKey(key))
+            {
+                Storage.Remove(key);
+            }
+        }
+
+        public void ClearEntries()
+        {
+            Storage.Clear();
+            Generated = DateTimeOffset.Now;
+            LastSaved = DateTimeOffset.MinValue;
+        }
+
+        #endregion
+
+        #region serialization
+
+        public void CheckForAutosave()
+        {
+            if (LastSaved.AddMinutes(5) < DateTimeOffset.Now)
+            {
+                Save();
+            }
+        }
+
+        /// <summary>
+        /// Saves the current object
+        /// </summary>
+        /// <returns>true if save was successfull</returns>
+        public void Save(string file = null)
+        {
+            if (String.IsNullOrWhiteSpace(file)) file = Filepath;
+
+            try
+            {
+                LastSaved = DateTimeOffset.Now;
+                var text = JsonConvert.SerializeObject(this, Formatting.Indented);
+                File.WriteAllText(file, text);
+            }
+            catch
+            {
+                try { File.Delete(file); } catch { }
+            }
+        }
+
+        /// <summary>
+        /// gets a storage instance, new one or from saved one
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public static JsonObjectStore<T> Load(string file)
+        {
+            if (File.Exists(file))
+            {
+                try
+                {
+                    var text = File.ReadAllText(file);
+                    var result = JsonConvert.DeserializeObject<JsonObjectStore<T>>(text);
+                    result.Filepath = file;
+                    return result;
+                }
+                catch { }
+            }
+
+            return new JsonObjectStore<T>
+            {
+                Filepath = file
+            };
+        }
+
+        #endregion
+
+
+
+
+    }
+}
