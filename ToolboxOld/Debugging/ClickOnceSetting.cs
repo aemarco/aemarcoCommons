69755e4a6fafc78d5db69b794c8f3865967709e3
aemarco <4aemarco@gmail.com>
2020-01-02 17:29:11 +0100

    added settingsmigration

---
+++ ToolboxOld/Debugging/ClickOnceSetting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolboxOld.Debugging
+{
+    public static class ClickOnceSetting
+    {
+        public static void EnsureMaintainUserSettings(this ApplicationSettingsBase settings, string keyName)
+        {
+            var keyValue = settings[keyName];
+            var defaultValue = settings.Properties[keyName].DefaultValue;
+            if (keyValue == defaultValue)
+            {
+                foreach (SettingsProperty prop in settings.Properties)
+                {
+                    if (prop.Attributes.ContainsKey(typeof(UserScopedSettingAttribute)))
+                    {
+                        object result = settings.GetPreviousVersion(prop.Name);
+                        settings[prop.Name] = result;
+                    }
+                }
+                settings.Save();
+            }
+        }
+    }
+}
