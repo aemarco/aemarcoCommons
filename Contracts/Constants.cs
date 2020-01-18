d076c91eeb1805ebdc6d3444188021e28cf7f676
aemarco <4aemarco@gmail.com>
2020-01-18 21:31:44 +0100

    now has constants for defaultUsers, watermark

--- Contracts/Constants.cs
+++ Contracts/Constants.cs
@@ -6,8 +6,10 @@ namespace Contracts
     {
         public static string WEBGIRLS_URL = "https://aemarco.myds.me";
 
-
-
+        //wallpaper
+        public static string CHANGER_EMAIL = "aeChanger@normal.com";
+        public static string DOWNLOADER_EMAIL = "aeChanger@normal.com";
+        public static string WATERMARK = "aemarco.myds.me";
 
         //video
         public static int TIME_UNTIL_VIDEOVIEW_COUTS_AS_PLAYED_SECONDS = 15;
