6e74c4a4cd7974f24283bdecf7277faa9e5f9815
aemarco <4aemarco@gmail.com>
2020-03-06 13:05:38 +0100

    added JWT Model

---
+++ Contracts/Api/JwtTokenModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Contracts.Api
+{
+    public class JwtTokenModel
+    {
+        public string UserId { get; set; }
+        public string Email { get; set; }
+        public int AdultLevel { get; set; }
+    }
+}
--- Contracts/Api/ResponseObjects/LoginResponse.cs
+++ Contracts/Api/ResponseObjects/LoginResponse.cs
@@ -4,6 +4,7 @@ namespace Contracts.Api.ResponseObjects
 {
     public class LoginResponse
     {
+        public string UserId { get; set; }
         public string Token { get; set; }
         public DateTimeOffset TokenValidUntil { get; set; }
     }
