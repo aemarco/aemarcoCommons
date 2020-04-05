084341d802adf9854e6fc87c6194a25ee0e658e9
aemarco <4aemarco@gmail.com>
2020-04-05 20:45:33 +0200

    now contains thumbnail queries

--- Contracts/Constants.cs
+++ Contracts/Constants.cs
@@ -4,7 +4,9 @@ namespace Contracts
 {
     public static class Constants
     {
+        // ReSharper disable StringLiteralTypo
         // ReSharper disable IdentifierTypo
+
 
         //webgirls
         public const string WebgirlsUrl = "https://aemarco.myds.me";
@@ -18,9 +20,14 @@ namespace Contracts
         //video
         public const int TimeUntilVideoViewCoutsAsPlayedSeconds = 15;
 
-        
+        //thumbnails
+        public const string ThumbnailSmall = "?width=300&roundedcorners=10&bgcolor=222";
+        public const string ThumbnailDetail = "?width=1110&roundedcorners=40&bgcolor=222";
+        public const string ThumbnailProfileSmall = "?height=450&roundedcorners=10&bgcolor=222";
+        public const string ThumbnailProfileDetail = "?height=600&roundedcorners=15&bgcolor=222";
 
 
+        // ReSharper restore StringLiteralTypo
         // ReSharper restore IdentifierTypo
 
     }
