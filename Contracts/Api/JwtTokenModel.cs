368a85575857748ef4c59dfea97c6f3451ff6b09
aemarco <4aemarco@gmail.com>
2020-04-08 18:41:10 +0200

    jwt changes

--- Contracts/Api/JwtTokenModel.cs
+++ Contracts/Api/JwtTokenModel.cs
@@ -1,9 +1,16 @@
-﻿namespace Contracts.Api
+﻿using Newtonsoft.Json;
+
+namespace Contracts.Api
 {
     public class JwtTokenModel
     {
+        [JsonProperty("sub")]
         public string UserId { get; set; }
+        [JsonProperty("email")]
         public string Email { get; set; }
+        
+
         public int AdultLevel { get; set; }
+        public bool IsSupervisor { get; set; }
     }
 }
