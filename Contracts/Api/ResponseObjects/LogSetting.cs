d77b8b26e3fe766682bf08f6b0b1d003a0f87e71
aemarco <4aemarco@gmail.com>
2020-04-30 16:01:46 +0200

    added log setting

---
+++ Contracts/Api/ResponseObjects/LogSetting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Contracts.Api.ResponseObjects
+{
+    public class LogSetting
+    {
+        /// <summary>
+        /// if logging to the api should be enabled
+        /// </summary>
+        public bool Enabled { get; set; }
+
+        /// <summary>
+        /// the minimum level, which should be logged to the api
+        /// </summary>
+        public LogLevel MinLevel { get; set; }
+
+    }
+
+}
