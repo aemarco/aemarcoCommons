59187e27120e49bee3ffdd457ea6a773eb86412c
aemarco <4aemarco@gmail.com>
2020-05-14 17:45:30 +0200

    cleanup

--- Contracts/Interfaces/StorageStuff.cs
+++ Contracts/Interfaces/StorageStuff.cs
@@ -18,12 +18,4 @@ namespace Contracts.Interfaces
         DateTimeOffset TimestampCreated { get; set; }
         DateTimeOffset TimestampSaved { get; set; }
     }
-
-    public abstract class BaseTypeToFileValue : ITypeToFileValue
-    {
-        public abstract string Filepath { get; }
-        public DateTimeOffset TimestampCreated { get; set; }
-        public DateTimeOffset TimestampSaved { get; set; }
-    }
-
 }
---
+++ Toolbox/SerializationTools/BaseTypeToFileValue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Contracts.Interfaces;
+
+namespace Toolbox.SerializationTools
+{
+    public abstract class BaseTypeToFileValue : ITypeToFileValue
+    {
+        public abstract string Filepath { get; }
+        public DateTimeOffset TimestampCreated { get; set; }
+        public DateTimeOffset TimestampSaved { get; set; }
+    }
+}
