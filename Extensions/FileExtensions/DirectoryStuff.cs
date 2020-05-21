962e51a2f7a3c4c615ea0f90c7193abbfe3ff561
aemarco <4aemarco@gmail.com>
2020-05-21 21:20:26 +0200

    more extensions

---
+++ Extensions/FileExtensions/DirectoryStuff.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Extensions.FileExtensions
+{
+    public static class DirectoryStuff
+    {
+        public static void TryDeleteEmptySubfolders(this DirectoryInfo dir)
+        {
+            TryDeleteEmptySubfolders(dir.FullName);
+        }
+
+        //so that is home is not visible to others
+        private static readonly string[] _ignoredFiles = 
+        {
+            "desktop.ini",
+            "Thumbs.db",
+        };
+
+        private static readonly string[] _ignoredExtensions = 
+        {
+            ".tmp"
+        };
+        
+        private static void TryDeleteEmptySubfolders(string path, bool isHome = true)
+        {
+            try
+            {
+                //for subfolder
+                foreach (var strSubDir in Directory.GetDirectories(path))
+                {
+                    //recursive for each subfolder
+                    if (!strSubDir.Contains("System Volume Information") &&
+                        !strSubDir.Contains("$RECYCLE.BIN") &&
+                        !strSubDir.Contains("#recycle"))
+                        TryDeleteEmptySubfolders(strSubDir, false);
+                }
+                // never delete home
+                if (isHome) return;
+                
+                
+                // delete from bottom up
+                if (!new DirectoryInfo(path).GetFiles("*", SearchOption.AllDirectories).Any(x =>
+                    !_ignoredFiles.Contains(x.Name) &&
+                    !_ignoredFiles.Contains(x.Extension.ToLower())))
+                {
+                    Directory.Delete(path, true);
+                }
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+    }
+}
