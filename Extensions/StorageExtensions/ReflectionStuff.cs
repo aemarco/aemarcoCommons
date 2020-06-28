9712f122ff090e560debf0cc8141a1c546c72c4f
aemarco <4aemarco@gmail.com>
2020-06-28 23:14:09 +0200

    added extensions

---
+++ Extensions/StorageExtensions/ReflectionStuff.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace Extensions.StorageExtensions
+{
+    public static class ReflectionStuff
+    {
+
+        public static string GetEntryDirectory()
+        {
+            return Assembly.GetEntryAssembly().GetDirectory();
+        }
+
+
+        public static string GetDirectory(this Assembly assembly)
+        {
+            var assemblyLocation = assembly.Location;
+            var di = new FileInfo(assemblyLocation).Directory;
+            return di!.FullName;
+        }
+
+    }
+}
