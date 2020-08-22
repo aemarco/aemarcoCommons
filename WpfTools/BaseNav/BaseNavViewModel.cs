fde07201289d6339bbd22258f2bfd6c5e8d81e2e
aemarco <4aemarco@gmail.com>
2020-08-23 01:23:42 +0200

    wpf tools refactorings

--- WpfTools/BaseNav/BaseNavViewModel.cs
+++ WpfTools/BaseNav/BaseNavViewModel.cs
@@ -1,7 +1,20 @@
 ﻿using WpfTools.BaseModels;
+using WpfTools.Commands;
 
 namespace WpfTools.BaseNav
 {
+    public interface INavView //inherit in specific interface for view
+    {
+        object DataContext { get; set; }
+
+    }
+    public interface INavViewModel : IBaseViewModel //inherit in specific interface for view model, inherit from BaseNaveViewModel
+    {
+        INavWindowViewModel Window { get; set; }
+        INavView View { get; set; }
+       
+    }
+
     public abstract class BaseNavViewModel : BaseViewModel, INavViewModel
     {
         protected BaseNavViewModel(INavView view)
@@ -12,5 +25,20 @@ namespace WpfTools.BaseNav
 
         public INavWindowViewModel Window { get; set; }
         public INavView View { get; set; }
+
+
+        public override DelegateCommand CloseCommand
+        {
+            get
+            {
+                return new DelegateCommand()
+                {
+                   CommandAction = () =>
+                   {
+                       Window.Window.Close();
+                   }
+                };
+            }
+        }
     }
 }
--- WpfTools/BaseNav/BaseNavWindowViewModel.cs
+++ WpfTools/BaseNav/BaseNavWindowViewModel.cs
@@ -2,6 +2,25 @@
 
 namespace WpfTools.BaseNav
 {
+
+    //windows
+    public interface INavMainWindow : INavWindow { } //implement this in the main window
+    public interface INavWindow //inherit this interface for other windows
+    {
+        object DataContext { get; set; }
+        void Close();
+    }
+
+    public interface INavMainViewModel : INavWindowViewModel { } //implement this in the main view model, inherit BaseNavWindowViewModel
+    public interface INavWindowViewModel : IBaseViewModel //inherit this for other window view models
+    {
+        INavWindow Window { get; set; }
+        INavView View { get; set; }
+        void ShowViewFor<T>() where T : INavViewModel;
+    }
+
+
+
     public abstract class BaseNavWindowViewModel : BaseViewModel, INavWindowViewModel
     {
         protected BaseNavWindowViewModel(INavWindow window)
