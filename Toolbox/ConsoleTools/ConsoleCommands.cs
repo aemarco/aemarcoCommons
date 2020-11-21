86c022f73ef705204be3f6fb3006b76678401bee
aemarco <4aemarco@gmail.com>
2020-11-22 00:13:02 +0100

    added console stuff

---
+++ Toolbox/ConsoleTools/ConsoleCommands.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace aemarcoCommons.Toolbox.ConsoleTools
+{
+    public static class ConsoleCommands
+    {
+        public static void ClearCurrentConsoleLine()
+        {
+            var currentLineCursor = Console.CursorTop;
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, currentLineCursor);
+        }
+
+
+    }
+}
