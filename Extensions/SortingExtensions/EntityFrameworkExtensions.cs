a3a6658bf052b6ac3f15dd1ed3243edf9103a86f
aemarco <4aemarco@gmail.com>
2020-12-29 01:27:59 +0100

    sorting extension

---
+++ Extensions/SortingExtensions/EntityFrameworkExtensions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace aemarcoCommons.Extensions.SortingExtensions
+{
+    public static class EntityFrameworkExtensions
+    {
+        public static IOrderedQueryable<T>  OrderBy<T>(this IQueryable<T> query, string propertyName)
+        {
+            var entityType = typeof(T);
+
+            //Create x=>x.PropName
+            var propertyInfo = entityType.GetProperty(propertyName);
+            ParameterExpression arg = Expression.Parameter(entityType, "x");
+            MemberExpression property = Expression.Property(arg, propertyName);
+            var selector = Expression.Lambda(property, new ParameterExpression[] { arg });
+
+            //Get System.Linq.Queryable.OrderBy() method.
+            var enumarableType = typeof(System.Linq.Queryable);
+            var method = enumarableType.GetMethods()
+                 .Where(m => m.Name == "OrderBy" && m.IsGenericMethodDefinition)
+                 .Where(m =>
+                 {
+                     var parameters = m.GetParameters().ToList();
+                     return parameters.Count == 2;
+                 }).Single();
+
+            //The linq's OrderBy<TSource, TKey> has two generic types, which provided here
+            MethodInfo genericMethod = method.MakeGenericMethod(entityType, propertyInfo.PropertyType);
+
+            // Call query.OrderBy(selector), with query and selector: x=> x.PropName
+            // Note that we pass the selector as Expression to the method and we don't compile it.
+            // By doing so EF can extract "order by" columns and generate SQL for it
+            var newQuery = (IOrderedQueryable<T>)genericMethod.Invoke(genericMethod, new object[] { query, selector });
+            return newQuery;
+        }
+
+        public static IOrderedQueryable<T> OrderByDescending<T>(this IQueryable<T> query, string propertyName)
+        {
+            var entityType = typeof(T);
+
+            //Create x=>x.PropName
+            var propertyInfo = entityType.GetProperty(propertyName);
+            ParameterExpression arg = Expression.Parameter(entityType, "x");
+            MemberExpression property = Expression.Property(arg, propertyName);
+            var selector = Expression.Lambda(property, new ParameterExpression[] { arg });
+
+            //Get System.Linq.Queryable.OrderBy() method.
+            var enumarableType = typeof(System.Linq.Queryable);
+            var method = enumarableType.GetMethods()
+                 .Where(m => m.Name == "OrderByDescending" && m.IsGenericMethodDefinition)
+                 .Where(m =>
+                 {
+                     var parameters = m.GetParameters().ToList();
+                     return parameters.Count == 2;
+                 }).Single();
+
+            //The linq's OrderByDescending<TSource, TKey> has two generic types, which provided here
+            MethodInfo genericMethod = method.MakeGenericMethod(entityType, propertyInfo.PropertyType);
+
+            // Call query.OrderByDescending(selector), with query and selector: x=> x.PropName
+            // Note that we pass the selector as Expression to the method and we don't compile it.
+            // By doing so EF can extract "order by" columns and generate SQL for it
+            var newQuery = (IOrderedQueryable<T>)genericMethod.Invoke(genericMethod, new object[] { query, selector });
+            return newQuery;
+        }
+    }
+}
