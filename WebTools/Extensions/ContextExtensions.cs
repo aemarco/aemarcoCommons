970792d14c485f9ea7a217da9f3e06d97e49bdbf
aemarco <aemarco@outlook.de>
2021-03-14 19:10:08 +0100

    started with web tools

---
+++ WebTools/Extensions/ContextExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+
+namespace aemarcoCommons.WebTools.Extensions
+{
+    public static class ContextExtensions
+    {
+        public static string GetRootPath(this HttpContext context)
+        {
+            var uri = $"{context.Request.Scheme}://{context.Request.Host.ToUriComponent()}";
+            return uri;
+        }
+
+        public static string GetBasePath(this HttpContext context)
+        {
+            var uri = $"{context.GetRootPath()}{context.Request.PathBase.ToUriComponent()}";
+            return uri;
+        }
+        
+        public static string GetAbsolutePath(this HttpContext context)
+        {
+            var uri = $"{context.GetBasePath()}{context.Request.Path.ToUriComponent()}{context.Request.QueryString.ToUriComponent()}";
+            return uri;
+        }
+
+        public static string GetAccessToken(this HttpContext context)
+        {
+            var token = context?.Request.Headers[HeaderNames.Authorization].ToString();
+            return token;
+        }
+    }
+}
