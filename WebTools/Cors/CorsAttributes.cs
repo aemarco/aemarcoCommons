aed6fbc5ffd213ca64e689d0b54030aaca3bf9ff
aemarco <aemarco@outlook.de>
2021-03-20 21:29:22 +0100

    added cors stuff

---
+++ WebTools/Cors/CorsAttributes.cs
@@ -0,0 +1,50 @@
+﻿using System;
+// ReSharper disable ClassNeverInstantiated.Global
+
+namespace aemarcoCommons.WebTools.Cors
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = true, Inherited = true)]
+    public class CorsOriginAttribute : Attribute
+    {
+        public string Origin { get; }
+
+        public CorsOriginAttribute(string origin)
+        {
+            Origin = origin;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = true, Inherited = true)]
+    public class CorsHeaderAttribute : Attribute
+    {
+        public string Header { get; }
+
+        public CorsHeaderAttribute(string header)
+        {
+            Header = header;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = true, Inherited = true)]
+    public class CorsMethodAttribute : Attribute
+    {
+        public string Method { get; }
+
+        public CorsMethodAttribute(string method)
+        {
+            Method = method;
+
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public class CorsPreflightMaxAgeAttribute : Attribute
+    {
+        public int PreflightMaxAge { get; }
+
+        public CorsPreflightMaxAgeAttribute(int preflightMaxAge)
+        {
+            PreflightMaxAge = preflightMaxAge;
+        }
+    }
+}
---
+++ WebTools/Cors/CorsExtensions.cs
@@ -0,0 +1,61 @@
+﻿using aemarcoCommons.Extensions.AttributeExtensions;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using System;
+using System.Linq;
+
+namespace aemarcoCommons.WebTools.Cors
+{
+    public static class CorsExtensions
+    {
+        public static CorsOptions AddCorsPolicies(this CorsOptions config, Type policyContainer)
+        {
+            foreach (var entry in policyContainer.GetFields())
+            {
+                var policyName = (string)entry.GetValue(null)!;
+                var origins = policyContainer
+                    .GetAttributes<CorsOriginAttribute>(entry.Name)
+                    .Select(x => x.Origin)
+                    .ToArray();
+                var headers = policyContainer
+                    .GetAttributes<CorsHeaderAttribute>(entry.Name)
+                    .Select(x => x.Header)
+                    .ToArray();
+                var methods = policyContainer
+                    .GetAttributes<CorsMethodAttribute>(entry.Name)
+                    .Select(x => x.Method)
+                    .ToArray();
+                var preflight = policyContainer
+                    .GetAttribute<CorsPreflightMaxAgeAttribute>(entry.Name)?
+                    .PreflightMaxAge;
+
+
+                config.AddPolicy(policyName, builder =>
+                {
+                    //origins
+                    if (origins.Length > 0)
+                        builder.WithOrigins(origins);
+                    else
+                        builder.AllowAnyOrigin();
+
+                    //header
+                    if (headers.Length > 0)
+                        builder.WithHeaders(headers);
+                    else
+                        builder.AllowAnyHeader();
+
+                    //methods
+                    if (methods.Length > 0)
+                        builder.WithMethods(methods);
+                    else
+                        builder.AllowAnyMethod();
+
+                    //preflight-cache
+                    if (preflight.HasValue)
+                        builder.SetPreflightMaxAge(TimeSpan.FromSeconds(preflight.Value));
+
+                });
+            }
+            return config;
+        }
+    }
+}
