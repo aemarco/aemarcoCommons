009be3e5b1102e3a5c5ab2c93b79eafdca92cd32
aemarco <aemarco@outlook.de>
2021-04-12 19:53:32 +0200

    WpfTools not Fontawesome

---
+++ WpfTools/Commands/BaseCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Input;
+#nullable enable
+
+namespace aemarcoCommons.WpfTools.Commands
+{
+    public abstract class BaseCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+        protected virtual void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+        public virtual bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+        public abstract void Execute(object? parameter);
+    }
+}
