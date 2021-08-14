bbe22fcb071fc1fb5698d7dac32838fba5b6dbea
aemarco <aemarco@outlook.de>
2021-08-14 07:56:18 +0200

    some more command

---
+++ WpfTools/Commands/ExitApplicationCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+#nullable enable
+
+namespace aemarcoCommons.WpfTools.Commands
+{
+    public class ExitApplicationCommand : BaseCommand
+    {
+        private readonly Action? _beforeClose;
+
+        public ExitApplicationCommand(Action? beforeClose = null)
+        {
+            _beforeClose = beforeClose;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            _beforeClose?.Invoke();
+            Application.Current.Shutdown();
+        }
+    }
+}
