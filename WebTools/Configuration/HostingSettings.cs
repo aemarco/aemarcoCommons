7fa893598604a9488b55fb5e44047c4c78a08ff8
aemarco <aemarco@outlook.de>
2021-09-26 06:17:58 +0200

    started with webgirls

---
+++ WebTools/Configuration/HostingSettings.cs
@@ -0,0 +1,36 @@
+﻿using aemarcoCommons.Toolbox.AppConfiguration;
+
+
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+
+namespace aemarcoCommons.WebTools.Configuration
+{
+    public class HostingSettings : SettingsBase
+    {
+
+        /// <summary>
+        /// Settings for JWT Checks
+        /// </summary>
+        public string? JwtAuthority { get; set; }
+        public string? JwtSecret { get; set; }
+
+
+        /// <summary>
+        /// Settings for oidc checks
+        /// </summary>
+        public string? OidcAuthority { get; set; }
+
+        public string? OidcClientId { get; set; }
+        public string? OidcClientSecret { get; set; }
+
+
+
+        /// <summary>
+        /// Enables DeveloperExceptionPage
+        /// </summary>
+        public bool UseDeveloperExceptionPage { get; set; }
+    }
+}
