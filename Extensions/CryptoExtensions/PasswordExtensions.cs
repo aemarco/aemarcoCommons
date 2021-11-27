71c96eaa6816f68f41c536e0780bfb468c17f508
aemarco <aemarco@outlook.de>
2021-11-27 21:01:33 +0100

    fixed crypto bugs

--- Extensions/CryptoExtensions/PasswordExtensions.cs
+++ Extensions/CryptoExtensions/PasswordExtensions.cs
@@ -8,85 +8,98 @@ namespace aemarcoCommons.Extensions.CryptoExtensions
 {
     public static class PasswordExtensions
     {
-        // This constant is used to determine the key size of the encryption algorithm in bits.
-        // We divide this by 8 within the code below to get the equivalent number of bytes.
+        //128, 192 or 256 bits
         private const int KeySize = 128;
 
         // This constant determines the number of iterations for the password bytes generation function.
         private const int DerivationIterations = 1000;
 
+
         public static string Encrypt(this string plainText, string passPhrase)
         {
-            // Salt and IV is randomly generated each time, but is prepended to encrypted cipher text
-            // so that the same Salt and IV values can be used when decrypting.  
-            var saltStringBytes = GenerateRandomEntropy();
-            var ivStringBytes = GenerateRandomEntropy();
-            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
-            using (var pwd = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations))
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
             {
-                var keyBytes = pwd.GetBytes(KeySize / 8);
-                using (var symmetricKey = new RijndaelManaged
-                {
-                    BlockSize = 128,
-                    Mode = CipherMode.CBC,
-                    Padding = PaddingMode.PKCS7
-                })
-                {
-                    using (var encryptor = symmetricKey.CreateEncryptor(keyBytes, ivStringBytes))
+                // Salt and IV is randomly generated each time, but is prepended to encrypted cipher text
+                // so that the same Salt and IV values can be used when decrypting.  
+                var saltStringBytes = GenerateRandomEntropy(aesAlg.BlockSize);
+                using (var pwd = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations))
+                { 
+                    aesAlg.KeySize = KeySize;
+                    aesAlg.Padding = PaddingMode.PKCS7;
+                    aesAlg.IV = GenerateRandomEntropy(aesAlg.BlockSize);
+                    aesAlg.Key = pwd.GetBytes(aesAlg.KeySize / 8);
+
+                    // Create an encryptor to perform the stream transform.
+                    ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                    var resultBytes = saltStringBytes;
+                    resultBytes = resultBytes.Concat(aesAlg.IV).ToArray();
+                    byte[] encrypted;
+                    // Create the streams used for encryption.
+                    using (MemoryStream msEncrypt = new MemoryStream())
                     {
-                        using (var memoryStream = new MemoryStream())
+                        using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
                         {
-                            using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                            using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
                             {
-                                cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
-                                cryptoStream.FlushFinalBlock();
-                                // Create the final bytes as a concatenation of the random salt bytes, the random iv bytes and the cipher bytes.
-                                var cipherTextBytes = saltStringBytes;
-                                cipherTextBytes = cipherTextBytes.Concat(ivStringBytes).ToArray();
-                                cipherTextBytes = cipherTextBytes.Concat(memoryStream.ToArray()).ToArray();
-                                memoryStream.Close();
-                                cryptoStream.Close();
-                                return Convert.ToBase64String(cipherTextBytes);
+                                //Write all data to the stream.
+                                swEncrypt.Write(plainText);
                             }
+                            encrypted = msEncrypt.ToArray();
                         }
                     }
+                    // concat the encrypted bytes from the memory stream.
+                    resultBytes = resultBytes.Concat(encrypted).ToArray();
+                   
+                    var result = Convert.ToBase64String(resultBytes);
+                    return result;
                 }
             }
         }
+
 
         public static string Decrypt(this string cipherText, string passPhrase)
         {
             // Get the complete stream of bytes that represent:
-            // [32 bytes of Salt] + [32 bytes of IV] + [n bytes of CipherText]
+            // [n bytes of Salt] + [n bytes of IV] + [n bytes of CipherText]
             var cipherTextBytesWithSaltAndIv = Convert.FromBase64String(cipherText);
-            // Get the salt bytes by extracting the first 32 bytes from the supplied cipherText bytes.
-            var saltStringBytes = cipherTextBytesWithSaltAndIv.Take(KeySize / 8).ToArray();
-            // Get the IV bytes by extracting the next 32 bytes from the supplied cipherText bytes.
-            var ivStringBytes = cipherTextBytesWithSaltAndIv.Skip(KeySize / 8).Take(KeySize / 8).ToArray();
-            // Get the actual cipher text bytes by removing the first 64 bytes from the cipherText string.
-            var cipherTextBytes = cipherTextBytesWithSaltAndIv.Skip((KeySize / 8) * 2).Take(cipherTextBytesWithSaltAndIv.Length - ((KeySize / 8) * 2)).ToArray();
 
-            using (var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations))
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
             {
-                var keyBytes = password.GetBytes(KeySize / 8);
-                using (var symmetricKey = new RijndaelManaged
+                // Get the salt bytes by extracting the first n bytes from the supplied cipherText bytes.
+                var saltStringBytes = cipherTextBytesWithSaltAndIv.Take(aesAlg.BlockSize / 8).ToArray();
+                using (var pwd = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations))
                 {
-                    BlockSize = 128,
-                    Mode = CipherMode.CBC,
-                    Padding = PaddingMode.PKCS7
-                })
-                {
-                    using (var decryptor = symmetricKey.CreateDecryptor(keyBytes, ivStringBytes))
+                    aesAlg.KeySize = KeySize;
+                    aesAlg.Padding = PaddingMode.PKCS7;
+                    aesAlg.IV = cipherTextBytesWithSaltAndIv.Skip(saltStringBytes.Length).Take(aesAlg.BlockSize / 8).ToArray();
+                    aesAlg.Key = pwd.GetBytes(aesAlg.KeySize / 8);
+                   
+                    // Get the actual cipher text bytes by removing the first n bytes from the cipherText string.
+                    var cipherTextBytes = cipherTextBytesWithSaltAndIv
+                        .Skip(saltStringBytes.Length)
+                        .Skip( aesAlg.IV.Length)
+                        .Take(cipherTextBytesWithSaltAndIv.Length - saltStringBytes.Length -  aesAlg.IV.Length).ToArray();
+
+                    // Create a decryptor to perform the stream transform.
+                    ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                    // Create the streams used for decryption.
+                    using (MemoryStream msDecrypt = new MemoryStream(cipherTextBytes))
                     {
-                        using (var memoryStream = new MemoryStream(cipherTextBytes))
+                        using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
                         {
-                            using (var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                            using (StreamReader srDecrypt = new StreamReader(csDecrypt))
                             {
-                                var plainTextBytes = new byte[cipherTextBytes.Length];
-                                var decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
-                                memoryStream.Close();
-                                cryptoStream.Close();
-                                return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+
+                                // Read the decrypted bytes from the decrypting stream
+                                // and place them in a string.
+                                string plaintext = srDecrypt.ReadToEnd();
+                                return plaintext;
                             }
                         }
                     }
@@ -94,9 +107,9 @@ namespace aemarcoCommons.Extensions.CryptoExtensions
             }
         }
 
-        private static byte[] GenerateRandomEntropy()
+        private static byte[] GenerateRandomEntropy(int size)
         {
-            var randomBytes = new byte[KeySize / 8];
+            var randomBytes = new byte[size / 8];
             using (var rngCsp = new RNGCryptoServiceProvider())
             {
                 // Fill the array with cryptographically secure random bytes.
---
+++ ExtensionsTests/CryptoExtensionsTests/PasswordExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using aemarcoCommons.Extensions.CryptoExtensions;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace ExtensionsTests.CryptoExtensionsTests
+{
+    public class PasswordExtensionsTests
+    {
+        [TestCase("test")]
+        [TestCase("77B80F6DE365762B070048D1F59ABB39")]
+        [TestCase(@"C:\Users\someUser\someDir\someOtherDirrrrrr\someTestDir\bin\Debug\net6.0\dataFolder\someFile-000-111.file")]
+        public void EncryptDecrypt_ShouldWork(string text)
+        {
+            var encrypted = text.Encrypt("password");
+            var result = encrypted.Decrypt("password");
+
+            result.Should().Be(text);
+        }
+    }
+}
