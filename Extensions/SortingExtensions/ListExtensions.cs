92d0a07d6ec64b9af7bb7d346d26f3c49e1f69e6
aemarco <aemarco@outlook.de>
2022-03-06 20:20:30 +0100

    chunking of lists

--- Extensions/SortingExtensions/ListExtensions.cs
+++ Extensions/SortingExtensions/ListExtensions.cs
@@ -6,11 +6,33 @@ namespace aemarcoCommons.Extensions.SortingExtensions
 {
     public static class ListExtensions
     {
-        private static readonly Random _random = new Random();
+        private static readonly Random Random = new Random();
         public static List<T> Shuffle<T>(this List<T> list)
         {
-            list = list.OrderBy(x => _random.Next()).ToList();
+            list = list.OrderBy(x => Random.Next()).ToList();
             return list;
         }
+
+        public static List<List<T>> Chunk<T>(this List<T> list, int chunkSize)
+        {
+            var copy = list.ToList();
+
+            if (chunkSize <= 0)
+            {
+                throw new ArgumentException("chunkSize must be greater than 0.");
+            }
+
+            List<List<T>> result = new List<List<T>>();
+            while (copy.Count > 0)
+            {
+                var chunk = copy.Take(chunkSize).ToList();
+                copy.RemoveRange(0, chunk.Count);
+                result.Add(chunk);
+            }
+            return result;
+        }
     }
+
+
+    
 }
