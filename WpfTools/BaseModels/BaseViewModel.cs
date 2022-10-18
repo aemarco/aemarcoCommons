d6574db73528174f969786adc87eecc0c614618b
aemarco <4aemarco@gmail.com>
2022-10-18 10:56:31 +0200

    wpfhelper now url navigate command

--- WpfTools/BaseModels/BaseViewModel.cs
+++ WpfTools/BaseModels/BaseViewModel.cs
@@ -1,9 +1,23 @@
-﻿using aemarcoCommons.WpfTools.Commands;
+﻿using aemarcoCommons.Extensions.NetworkExtensions;
+using aemarcoCommons.WpfTools.Commands;
+using System;
+using System.Windows.Input;
 
 namespace aemarcoCommons.WpfTools.BaseModels;
 
 public class BaseViewModel : BaseNotifier, IBaseViewModel
 {
     public virtual DelegateCommand CloseCommand { get; } = new DelegateCommand();
+
+    public virtual ICommand NavigateToUrlCommand =>
+        new DelegateCommand
+        {
+            CommandAction = x =>
+            {
+                if (x is not string url)
+                    return;
+                new Uri(url).OpenInBrowser();
+            }
+        };
 
 }
