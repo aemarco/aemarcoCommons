bdcda5903ab7a1dae0404bd0d381dff2b556ecbb
aemarco <aemarco>
2023-12-26 17:45:39 +0100

    added Progress Wrapper

---
+++ ConsoleTools/Power/Spectre/Progress.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading.Tasks;
+
+// ReSharper disable once CheckNamespace
+namespace aemarcoCommons.ConsoleTools;
+
+public static partial class PowerConsole
+{
+
+    public static void StartProgress(Action<ProgressContext> work)
+    {
+        AnsiConsole.Progress()
+            .Columns(
+                new TaskDescriptionColumn(),
+                new ProgressBarColumn(),
+                new PercentageColumn(),
+                new RemainingTimeColumn(),
+                new SpinnerColumn())
+            .Start(work);
+    }
+
+    public static T StartProgress<T>(Func<ProgressContext, T> work)
+    {
+        return AnsiConsole.Progress()
+            .Columns(
+                new TaskDescriptionColumn(),
+                new ProgressBarColumn(),
+                new PercentageColumn(),
+                new RemainingTimeColumn(),
+                new SpinnerColumn())
+            .Start(work);
+    }
+
+
+    public static async Task StartProgressAsync(Func<ProgressContext, Task> work)
+    {
+        await AnsiConsole.Progress()
+            .Columns(
+                new TaskDescriptionColumn(),
+                new ProgressBarColumn(),
+                new PercentageColumn(),
+                new RemainingTimeColumn(),
+                new SpinnerColumn())
+            .StartAsync(work);
+    }
+
+    public static async Task<T> StartProgressAsync<T>(Func<ProgressContext, Task<T>> work)
+    {
+        return await AnsiConsole.Progress()
+            .Columns(
+                new TaskDescriptionColumn(),
+                new ProgressBarColumn(),
+                new PercentageColumn(),
+                new RemainingTimeColumn(),
+                new SpinnerColumn())
+            .StartAsync(work);
+    }
+
+}
