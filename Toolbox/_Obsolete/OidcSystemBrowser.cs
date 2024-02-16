76974bb5357786ade662b03d08f055e2bda78e92
aemarco <aemarco>
2024-02-16 16:35:31 +0100

    fixes

--- Toolbox/_Obsolete/OidcSystemBrowser.cs
+++ Toolbox/_Obsolete/OidcSystemBrowser.cs
@@ -6,7 +6,7 @@ namespace aemarcoCommons.Toolbox.Oidc
     [Obsolete("Use SystemBrowser instead")]
     public class OidcSystemBrowser : SystemBrowser
     {
-        public OidcSystemBrowser(int? port, string postLoginUrl = null)
+        public OidcSystemBrowser(int port, string postLoginUrl = null)
             : base(port, postLoginUrl)
         {
         }
