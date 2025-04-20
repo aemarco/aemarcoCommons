b28bc6145fb7d673bc0f798b6f0626b3bab39e1c
aemarco <aemarco>
2025-04-20 15:55:54 +0200

    c

---
+++ ConsoleTools/Power/Spectre/Json.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using Spectre.Console.Json;
+
+// ReSharper disable once CheckNamespace
+namespace aemarcoCommons.ConsoleTools;
+
+public static partial class PowerConsole
+{
+    public static void WriteAsJson(object o)
+    {
+        AnsiConsole.Write(new JsonText(JsonConvert.SerializeObject(o)));
+    }
+
+}
---
+++ ConsoleTools/Power/Spectre/Status.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+// ReSharper disable once CheckNamespace
+namespace aemarcoCommons.ConsoleTools;
+
+public static partial class PowerConsole
+{
+    public static async Task<T> StartStatusAsync<T>(
+        Func<StatusContext, Task<T>> work)
+    {
+        return await AnsiConsole.Status()
+            .SpinnerStyle(Style.Parse("green"))
+            .StartAsync(
+                "[purple]Doing stuff...[/]",
+                work);
+    }
+}
