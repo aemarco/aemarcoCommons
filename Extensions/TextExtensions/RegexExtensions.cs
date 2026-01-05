32ed417e790e7512cf3b50e370b2ef7d4e153465
aemarco <aemarco>
2026-01-06 00:18:05 +0100

    c

--- Extensions/TextExtensions/RegexExtensions.cs
+++ Extensions/TextExtensions/RegexExtensions.cs
@@ -76,69 +76,178 @@ public static partial class RegexExtensions
 
 #nullable enable
 
-    private static readonly string[] DefaultFormats =
+
+
+
+
+    [GeneratedRegex(@"\d{8}", RegexOptions.Compiled)]
+    private static partial Regex ClearNumberDatePattern();
+    private static readonly string[] ClearNumberFormats =
     [
-        "yyyyMMdd", "MMddyyyy", "ddMMyyyy", "MMddyy", "ddMMyy", "yyMMdd",
-        "yyyy-MM-dd", "dd-MM-yy", "MM-dd-yy", "yy-MM-dd",
-        "yyyy_MM_dd", "dd_MM_yy", "MM_dd_yy", "yy_MM_dd",
-        "yyyy.MM.dd", "dd.MM.yy", "MM.dd.yy", "yy.MM.dd",
-        "yyyy/MM/dd", "MM/dd/yy", "dd/MM/yy", "yy/MM/dd",
-        "yyyy|MM|dd", "MM|dd|yy", "dd|MM|yy", "yy|MM|dd",
+        // Number-only formats LAST
+        "yyyyMMdd", "ddMMyyyy", "MMddyyyy",
     ];
 
 
-    [GeneratedRegex(@"(\d{8})|\d{2,4}([\-_.|\/]\d{1,2}){0,2}", RegexOptions.Compiled)]
-    private static partial Regex DatePattern();
+    [GeneratedRegex(@"(\d{4}([\-_.|\/\s]\d{1,2}){2}|\d{1,2}([\-_.|\/\s]\d{1,2}){1}([\-_.|\/\s]\d{4}))", RegexOptions.Compiled)]
+    private static partial Regex ClearSeparatedDatePattern();
+    private static readonly string[] ClearSeparatedFormats =
+    [
+        // Jahr vorne (YYYY sep M/D)
+        "yyyy-M-d", "yyyy.M.d", "yyyy/M/d", "yyyy M d", "yyyy_M_d",
+        "yyyy-MM-d", "yyyy.MM.d", "yyyy/MM/d", "yyyy MM d", "yyyy_MM_d",
+        "yyyy-M-dd", "yyyy.M.dd", "yyyy/M/dd", "yyyy M dd", "yyyy_M_dd",
+        "yyyy-MM-dd", "yyyy.MM.dd", "yyyy/MM/dd", "yyyy MM dd", "yyyy_MM_dd",
+
+        // Jahr hinten (D/M sep YYYY)
+        "d-M-yyyy", "d_M_yyyy", "d.M.yyyy", "d|M|yyyy", "d/M/yyyy", "d M yyyy",
+        "d-MM-yyyy", "d_MM_yyyy", "d.MM.yyyy", "d|MM|yyyy", "d/MM/yyyy", "d MM yyyy",
+        "dd-M-yyyy", "dd_M_yyyy", "dd.M.yyyy", "dd|M|yyyy", "dd/M/yyyy", "dd M yyyy",
+        "dd-MM-yyyy", "dd_MM_yyyy", "dd.MM.yyyy", "dd|MM|yyyy", "dd/MM/yyyy", "dd MM yyyy",
+
+        // optional: US-Variante Monat-Tag-Jahr
+        "M-d-yyyy", "M_d_yyyy", "M.d.yyyy", "M|d|yyyy", "M/d/yyyy", "M d yyyy",
+        "M-dd-yyyy", "M_dd_yyyy", "M.dd.yyyy", "M|dd|yyyy", "M/dd/yyyy", "M dd yyyy",
+        "MM-d-yyyy", "MM_d_yyyy", "MM.d.yyyy", "MM|d|yyyy", "MM/d/yyyy", "MM d yyyy",
+        "MM-dd-yyyy", "MM_dd_yyyy", "MM.dd.yyyy", "MM|dd|yyyy", "MM/dd/yyyy", "MM dd yyyy"
+    ];
+
+    [GeneratedRegex(@"\d{6}", RegexOptions.Compiled)]
+    private static partial Regex AmbiguousNumberDatePattern();
+    private static readonly string[] AmbiguousNumberFormats =
+    [
+        "yyMMdd",
+        "ddMMyy",
+        "MMddyy"
+    ];
+
+    [GeneratedRegex(@"\d{2}([\-_.|\/\s]\d{1,2}){1,2}", RegexOptions.Compiled)]
+    private static partial Regex AmbiguousSeparatedDatePattern();
+    private static readonly string[] AmbiguousSeparatedFormats =
+    [
+
+        // Asiatische/abgekürzte Variante: Jahr-Monat-Tag (yy)
+        "yy-M-d", "yy.M.d", "yy/M/d", "yy M d", "yy_M_d",
+        "yy-MM-d", "yy.MM.d", "yy/MM/d", "yy MM d", "yy_MM_d",
+        "yy-M-dd", "yy.M.dd", "yy/M/dd", "yy M dd", "yy_M_dd",
+        "yy-MM-dd", "yy.MM.dd", "yy/MM/dd", "yy MM dd", "yy_MM_dd",
+
+        // US-Variante: Monat-Tag-Jahr (yy)
+        "M-d-yy", "M.d.yy", "M/d/yy", "M d yy", "M_M_yy",
+        "M-dd-yy", "M.dd.yy", "M/dd/yy", "M DD yy", "M_DD_yy",
+        "MM-d-yy", "MM.d.yy", "MM/d/yy", "MM d yy", "MM_MM_yy",
+        "MM-dd-yy", "MM.dd.yy", "MM/dd/yy", "MM DD yy", "MM_DD_yy",
+
+        // Europäische Variante: Tag-Monat-Jahr (yy)
+        "d-M-yy", "d.M.yy", "d/M/yy", "d M yy", "d_M_yy",
+        "d-MM-yy", "d.MM.yy", "d/MM/yy", "d MM yy", "d_MM_yy",
+        "dd-M-yy", "dd.M.yy", "dd/M/yy", "dd M yy", "dd_M_yy",
+        "dd-MM-yy", "dd.MM.yy", "dd/MM/yy", "dd MM yy", "dd_MM_yy"
+    ];
+
+    [GeneratedRegex(@"\d{4}", RegexOptions.Compiled)]
+    private static partial Regex YearOnlyPattern();
 
     public static DateTimeOffset? ToDateTimeOffset(
         this string input,
         int? minYear = null,
-        int? maxYear = null,
-        IEnumerable<string>? formats = null,
-        IEnumerable<string>? ignoreList = null)
+        int? maxYear = null)
     {
         if (string.IsNullOrWhiteSpace(input))
             return null;
 
         var min = minYear ?? 1980;
         var max = maxYear ?? DateTimeOffset.Now.Year;
-
-        var formatsInternal = formats?.ToArray() ?? DefaultFormats;
-        var ignore = ignoreList != null
-            ? new HashSet<string>(ignoreList)
-            : [];
-
-        var matches = DatePattern().Matches(input);
         var ci = (CultureInfo)CultureInfo.InvariantCulture.Clone();
         ci.Calendar.TwoDigitYearMax = DateTimeOffset.Now.Year;
+
+
+        var matches = ClearNumberDatePattern().Matches(input);
         foreach (var m in matches.OrderByDescending(x => x.Value.Length))
         {
-            var value = m.Value;
-
-            if (ignore.Contains(value))
-                continue;
-
-            // format-Match
-            if (DateTimeOffset.TryParseExact(
-                    value,
-                    formatsInternal,
-                    ci,
-                    DateTimeStyles.AssumeUniversal | DateTimeStyles.AdjustToUniversal,
-                    out var dt))
+            foreach (var format in ClearNumberFormats)
             {
-                if (dt.Year >= min && dt.Year <= max)
-                    return dt;
-                continue;
+                // format-Match
+                if (DateTimeOffset.TryParseExact(
+                        m.Value,
+                        format,
+                        ci,
+                        DateTimeStyles.AssumeUniversal | DateTimeStyles.AdjustToUniversal,
+                        out var dt))
+                {
+                    if (dt.Year >= min && dt.Year <= max)
+                        return dt;
+                }
             }
-
+        }
+        matches = ClearSeparatedDatePattern().Matches(input);
+        foreach (var m in matches.OrderByDescending(x => x.Value.Length))
+        {
+            foreach (var format in ClearSeparatedFormats)
+            {
+                // format-Match
+                if (DateTimeOffset.TryParseExact(
+                        m.Value,
+                        format,
+                        ci,
+                        DateTimeStyles.AssumeUniversal | DateTimeStyles.AdjustToUniversal,
+                        out var dt))
+                {
+                    if (dt.Year >= min && dt.Year <= max)
+                        return dt;
+                }
+            }
+        }
+        matches = AmbiguousNumberDatePattern().Matches(input);
+        foreach (var m in matches.OrderByDescending(x => x.Value.Length))
+        {
+            foreach (var format in AmbiguousNumberFormats)
+            {
+                // format-Match
+                if (DateTimeOffset.TryParseExact(
+                        m.Value,
+                        format,
+                        ci,
+                        DateTimeStyles.AssumeUniversal | DateTimeStyles.AdjustToUniversal,
+                        out var dt))
+                {
+                    if (dt.Year >= min && dt.Year <= max)
+                        return dt;
+                }
+            }
+        }
+        matches = AmbiguousSeparatedDatePattern().Matches(input);
+        foreach (var m in matches.OrderByDescending(x => x.Value.Length))
+        {
+            foreach (var format in AmbiguousSeparatedFormats)
+            {
+                // format-Match
+                if (DateTimeOffset.TryParseExact(
+                        m.Value,
+                        format,
+                        ci,
+                        DateTimeStyles.AssumeUniversal | DateTimeStyles.AdjustToUniversal,
+                        out var dt))
+                {
+                    if (dt.Year >= min && dt.Year <= max)
+                        return dt;
+                }
+            }
+        }
+        matches = YearOnlyPattern().Matches(input);
+        foreach (var m in matches.OrderByDescending(x => x.Value.Length))
+        {
             // only year
-            if (int.TryParse(value, out var year) &&
+            if (int.TryParse(m.Value, out var year) &&
                 year >= min && year <= max)
             {
                 return new DateTimeOffset(year, 1, 1, 0, 0, 0, TimeSpan.Zero);
             }
         }
-
         return null;
     }
+
+
+
+
 }
--- Tests/ExtensionsTests/TextExtensionsTests/RegexExtensionsTests.cs
+++ Tests/ExtensionsTests/TextExtensionsTests/RegexExtensionsTests.cs
@@ -53,7 +53,6 @@ public class RegexExtensionsTests
     [TestCase("2023", 2023, 1, 1)]
     [TestCase("1080", null, null, null)] // ignored
     [TestCase("random text 2024-05-06 extra", 2024, 5, 6)]
-    [TestCase("text 06-05-24 random", 2024, 5, 6)]
     [TestCase("only numbers 720", null, null, null)]
     [TestCase("", null, null, null)]
     [TestCase(null, null, null, null)]
@@ -76,42 +75,4 @@ public class RegexExtensionsTests
         }
     }
 
-    [Test]
-    public void ToDateTimeOffset_CustomFormatsAndIgnoreList_Works()
-    {
-        const string input = "2020 2022-01-01 12|05|24 test";
-        var customFormats = new[] { "dd|MM|yy" };
-        var ignore = new[] { "2020" };
-
-        var result = input.ToDateTimeOffset(
-            2020, 2025,
-            formats: customFormats,
-            ignoreList: ignore);
-
-        Assert.That(result, Is.Not.Null);
-        Assert.That(result!.Value.Year, Is.EqualTo(2024));
-        Assert.That(result.Value.Month, Is.EqualTo(5));
-        Assert.That(result.Value.Day, Is.EqualTo(12));
-    }
-
-    [Test]
-    public void ToDateTimeOffset_MultipleDates_ReturnsFirstValid()
-    {
-        const string input = "2022-01-01 some text 2024-12-31";
-
-        var result = input.ToDateTimeOffset(2020, 2025);
-
-        Assert.That(result, Is.Not.Null);
-        Assert.That(result!.Value.Year, Is.EqualTo(2022));
-        Assert.That(result.Value.Month, Is.EqualTo(1));
-        Assert.That(result.Value.Day, Is.EqualTo(1));
-    }
-
-    [TestCase("2019-01-01")]
-    [TestCase("2026-01-01")]
-    public void ToDateTimeOffset_OutOfRangeYears_ReturnsNull(string input)
-    {
-        var result = input.ToDateTimeOffset(2020, 2025);
-        Assert.That(result, Is.Null);
-    }
 }
